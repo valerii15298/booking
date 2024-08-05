6706e296a0e08c11790871739acb4e4204c82e4c
Valerii Petryniak <valerii15298@gmail.com>
2024-08-05 07:16:15 +0300

    add basic create booking form

--- web/src/pages/CreateBooking.tsx
+++ web/src/pages/CreateBooking.tsx
@@ -1,3 +1,7 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useId } from "react";
+import { useForm } from "react-hook-form";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -8,24 +12,96 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import type { Types } from "@/zod";
+import { Form, FormField } from "@/components/ui/form";
+import { type Types, zod } from "@/zod";
 
+function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}` as const;
+}
+
+function formatDateTime(date: Date) {
+  const dateStr = formatDate(date);
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${dateStr}T${hours}:${minutes}` as const;
+}
 export function CreateBooking({ id, name }: Types.Asset) {
+  const form = useForm({
+    defaultValues: {
+      from: new Date(),
+      to: new Date(),
+      assetId: id,
+    },
+    resolver: zodResolver(zod.bookingInput),
+  });
+
+  const formId = useId();
   return (
     <Dialog key={id}>
       <DialogTrigger asChild>
         <Button className="w-full rounded-none">{name}</Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="w-fit max-w-full overflow-x-auto">
         <DialogHeader>
           <DialogTitle>Create Booking</DialogTitle>
           <DialogDescription>
             Book <b>{name}</b> for a specific date and time
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">Form placeholder</div>
+        <Form {...form}>
+          <form
+            id={formId}
+            onSubmit={(e) => {
+              void form.handleSubmit((_data, e) => {
+                e?.preventDefault();
+                // console.log(data);
+              })(e);
+            }}
+            className="flex flex-col items-center gap-3 sm:flex-row sm:justify-between"
+          >
+            <input type="text" className="hidden" />
+            <FormField
+              control={form.control}
+              name="from"
+              render={({ field }) => (
+                <input
+                  type="datetime-local"
+                  className="w-min"
+                  {...field}
+                  value={formatDateTime(field.value)}
+                  onChange={(e) => {
+                    field.onChange(new Date(e.target.value));
+                  }}
+                />
+              )}
+            />
+            <span className="hidden whitespace-nowrap sm:inline">{"->"}</span>
+            <FormField
+              control={form.control}
+              name="to"
+              render={({ field }) => (
+                <input
+                  type="datetime-local"
+                  className="w-min"
+                  {...field}
+                  value={formatDateTime(field.value)}
+                  onChange={(e) => {
+                    field.onChange(new Date(e.target.value));
+                  }}
+                />
+              )}
+            />
+          </form>
+        </Form>
         <DialogFooter>
-          <Button type="submit">Create</Button>
+          <Button form={formId} type="submit">
+            Create
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
