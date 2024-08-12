ad2d1ca450255c2bf5716695b6abdb031a5d2b45
Valerii Petryniak <valerii15298@gmail.com>
2024-08-12 19:19:17 +0300

    api: db: add relations to drizzle schema

--- api/src/schema.ts
+++ api/src/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import * as pgCore from "drizzle-orm/pg-core";
 
 export const assets = pgCore.pgTable("assets", {
@@ -14,3 +15,11 @@ export const bookings = pgCore.pgTable("bookings", {
     .notNull()
     .references(() => assets.id),
 });
+
+export const assetsRelations = relations(assets, ({ many }) => ({
+  bookings: many(bookings),
+}));
+
+export const bookingsRelations = relations(bookings, ({ one }) => ({
+  asset: one(assets),
+}));
