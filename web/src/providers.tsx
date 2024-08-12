6802fd3caf6e68f895edf4cb9dc269538d61a132
Valerii Petryniak <valerii15298@gmail.com>
2024-08-12 19:25:09 +0300

    extract queryClient and trpcCleint to use trpcUtils

--- web/src/providers.tsx
+++ web/src/providers.tsx
@@ -1,22 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink, loggerLink } from "@trpc/client";
-import { useState } from "react";
-import superjson from "superjson";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import { TooltipProvider } from "./components/ui/tooltip";
-import { trpc } from "./trpc";
+import { queryClient, trpc, trpcClient } from "./trpc";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        loggerLink(),
-        httpBatchLink({ url: "/trpc", transformer: superjson }),
-      ],
-    }),
-  );
-
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
--- web/src/trpc.ts
+++ web/src/trpc.ts
@@ -1,7 +1,26 @@
-import { createTRPCReact } from "@trpc/react-query";
+import { QueryClient } from "@tanstack/react-query";
+import {
+  createTRPCQueryUtils,
+  createTRPCReact,
+  httpBatchLink,
+  loggerLink,
+} from "@trpc/react-query";
+import superjson from "superjson";
 
 import type { AppRouter } from "../../api/src/trpc";
 
 export const trpc = createTRPCReact<AppRouter>();
 
 export type { AppRouter };
+
+export const queryClient = new QueryClient();
+export const trpcClient = trpc.createClient({
+  links: [
+    loggerLink(),
+    httpBatchLink({ url: "/trpc", transformer: superjson }),
+  ],
+});
+export const trpcUtils = createTRPCQueryUtils({
+  client: trpcClient,
+  queryClient,
+});
