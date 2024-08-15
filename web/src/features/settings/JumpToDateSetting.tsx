5689fb5ee76b776c0aadb96c45e7d52f384b9f4a
Valerii Petryniak <valerii15298@gmail.com>
2024-08-16 02:08:21 +0300

    web: jump to date: add behaviour

--- web/src/features/settings/JumpToDateSetting.tsx
+++ web/src/features/settings/JumpToDateSetting.tsx
@@ -1,18 +1,27 @@
-import { formatDateTime } from "@/atoms/dates";
+import { getRouteApi } from "@tanstack/react-router";
+
+import { dateFromISO, dateToISO, formatDateTime } from "@/atoms/dates";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+const routeApi = getRouteApi("/");
 export function JumpToDateSetting() {
+  const navigate = routeApi.useNavigate();
+  const { date: dateRaw } = routeApi.useSearch();
+  const date = dateRaw ? new Date(dateToISO(dateRaw)) : new Date();
   return (
     <Label className="flex items-center justify-between">
       Jump To Date
       <Input
         type="datetime-local"
         className="w-fit"
-        defaultValue={formatDateTime(new Date())}
+        value={formatDateTime(date)}
         onChange={(e) => {
-          // eslint-disable-next-line no-console
-          console.log(new Date(e.target.value));
+          void navigate({
+            search: {
+              date: dateFromISO(new Date(e.target.value).toISOString()),
+            },
+          });
         }}
       />
     </Label>
