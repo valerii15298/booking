6b3397d1d86f5ca65f3b1be043016f0467a7e225
Valerii Petryniak <valerii15298@gmail.com>
2024-08-25 16:20:58 +0300

    date item tooltip for mobile

--- web/src/features/DateItem.tsx
+++ web/src/features/DateItem.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Tooltip,
   TooltipContent,
@@ -14,6 +16,7 @@ export function DateItem({
   first?: boolean;
 }) {
   const { dateItemHeight, menuPosition, dateDelimiter } = useApp();
+  const [tooltipOpen, setTooltipOpen] = useState(false);
 
   const isBreakpoint = dateDelimiter.isBreakpoint(date);
   const dateBreakpoint =
@@ -22,10 +25,14 @@ export function DateItem({
   const top =
     menuPosition === "top" ? 56 - dateItemHeight : 20 - dateItemHeight;
 
+  const [dateLocal, timeLocal] = date.toLocaleString().split(", ", 2);
   return (
-    <Tooltip>
+    <Tooltip open={tooltipOpen} onOpenChange={setTooltipOpen}>
       <TooltipTrigger asChild>
         <li
+          onClick={() => {
+            setTooltipOpen(true);
+          }}
           className={`flex flex-col items-end justify-end overflow-hidden border-b bg-background px-2 ${dateBreakpoint ? "sticky" : ""}`}
           style={{ height: dateItemHeight, top }}
         >
@@ -34,8 +41,8 @@ export function DateItem({
         </li>
       </TooltipTrigger>
       <TooltipPortal>
-        <TooltipContent className="w-24" side="right">
-          {date.toLocaleString()}
+        <TooltipContent side="right">
+          {dateLocal}, {<br />} {timeLocal}
         </TooltipContent>
       </TooltipPortal>
     </Tooltip>
