cbf037898c0a56284df5dfc68c4b333a8df6261d
Valerii Petryniak <valerii15298@gmail.com>
2024-08-25 15:53:38 +0300

    date item tooltip for desktop

--- web/src/features/Bookings.tsx
+++ web/src/features/Bookings.tsx
@@ -1,4 +1,10 @@
 import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipPortal,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useApp } from "@/features/app/useApp";
 import { trpc } from "@/trpc";
 
@@ -17,13 +23,22 @@ function DateItem({ date, first = false }: { date: Date; first?: boolean }) {
     menuPosition === "top" ? 56 - dateItemHeight : 20 - dateItemHeight;
 
   return (
-    <li
-      className={`flex flex-col items-end justify-end overflow-hidden border-b bg-background px-2 ${dateBreakpoint ? "sticky" : ""}`}
-      style={{ height: dateItemHeight, top }}
-    >
-      {dateBreakpoint}
-      {!dateBreakpoint && dateDelimiter.item(date)}
-    </li>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <li
+          className={`flex flex-col items-end justify-end overflow-hidden border-b bg-background px-2 ${dateBreakpoint ? "sticky" : ""}`}
+          style={{ height: dateItemHeight, top }}
+        >
+          {dateBreakpoint}
+          {!dateBreakpoint && dateDelimiter.item(date)}
+        </li>
+      </TooltipTrigger>
+      <TooltipPortal>
+        <TooltipContent className="w-24" side="right">
+          {date.toLocaleString()}
+        </TooltipContent>
+      </TooltipPortal>
+    </Tooltip>
   );
 }
 
